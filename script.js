67907d99af0733576b383fe758079d605d94eec4
saurbre8 <148504214+saurbre8@users.noreply.github.com>
2023-10-20 01:09:30 -0400

    output now replaces old

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ function returnBefore(){
     let neededGrade = (parseFloat(desiredGrade) - ((1 - adjustedExamPercent) * parseFloat(currentGrade))) / adjustedExamPercent;
     
     neededGrade = neededGrade.toFixed(2);
-    let resultElement = document.createElement("p");
+    let resultElement = document.getElementById("result");
+    //let resultElement = document.createElement("p");
+    if (!resultElement) {
+        resultElement = document.createElement("p");
+        resultElement.id = "result";
+    }
     if (isNaN(neededGrade)) {
         resultElement.textContent = "Please enter valid input.";
     } else if (neededGrade <= 0) {
@@ -18,7 +23,6 @@ function returnBefore(){
     else {
         resultElement.textContent = "You need to score at least a " + neededGrade + "% on your final to get your desired grade.";
     }
-    resultElement.id = "result";
     document.body.appendChild(resultElement);
 }
 
@@ -33,13 +37,16 @@ function returnAfter(){
     let finalGrade = (parseFloat(currentGrade) * (1 - adjustedExamPercent)) + (parseFloat(examGradeGot) * adjustedExamPercent)
     
     finalGrade = finalGrade.toFixed(2);
-    let resultElement = document.createElement("p");
+    let resultElement = document.getElementById("result");
+    if (!resultElement) {
+        resultElement = document.createElement("p");
+        resultElement.id = "result";
+    }
     if (isNaN(finalGrade)) {
         resultElement.textContent = "Please enter valid input.";
     }
     else {
         resultElement.textContent = "You scored a " + finalGrade + "% in the class.";
     }
-    resultElement.id = "result";
     document.body.appendChild(resultElement);
 }
